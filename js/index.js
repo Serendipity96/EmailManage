8a401a017438f1dedb0f814ebbbf22afad98fbb9
Serendipityy96 <justserendipity96@gmail.com>
2018-04-25 12:45:49 +0800

    添加3个管理页面

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
-$('li').on('click',function (currentClass) {
-    if ( currentClass !== "active" ) {
+$('li').on('click', function (currentClass) {
+    if (currentClass !== "active") {
         $(this).addClass('active');
         $(this).siblings().removeClass('active');
     }
 });
-
