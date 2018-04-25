36beffbbf21c2b8eb4175c2ece6923ea515c18e3
Serendipityy96 <justserendipity96@gmail.com>
2018-04-25 20:58:58 +0800

    完成后台管理界面和简单的JS逻辑

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
+var layoutArr = document.querySelector('body').children[1].children;
+
 $('li').on('click', function (currentClass) {
     if (currentClass !== "active") {
         $(this).addClass('active');
         $(this).siblings().removeClass('active');
+        var layoutName = '.'+this.id+'-layout';
+        for (let i = 0; i < layoutArr.length; i++) {
+            layoutArr[i].style.display = 'none'
+        }
+        document.querySelector(layoutName).style.display = 'block';
     }
+
 });
+
